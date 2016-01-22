7adffc1bf8f1fd0c791fcb037d6135ea356b18bf
louisbl <louis@beltramo.me>
2016-01-22 13:49:56 +0100

    Add Home fragment with button and callbacks

--- app/src/main/java/fr/louisbl/hellomap/HomeActivity.java
+++ app/src/main/java/fr/louisbl/hellomap/HomeActivity.java
@@ -1,13 +1,32 @@
 package fr.louisbl.hellomap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements HomeFragment.HomeFragmentListener {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
+
+        changeFragment(new HomeFragment());
+    }
+
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.home_container, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onButtonMapClick() {
+        Log.d(TAG, "on button clicked");
     }
 }
---
+++ app/src/main/java/fr/louisbl/hellomap/HomeFragment.java
@@ -0,0 +1,65 @@
+package fr.louisbl.hellomap;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HomeFragmentListener} interface
+ * to handle interaction events.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private HomeFragmentListener mListener;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button = (Button) view.findViewById(R.id.home_button_map);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof HomeFragmentListener) {
+            mListener = (HomeFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement HomeFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mListener != null) {
+            mListener.onButtonMapClick();
+        }
+    }
+
+    public interface HomeFragmentListener {
+        void onButtonMapClick();
+    }
+}

3075a10cece48d042a2bea2b7c62d66ebb109f25
louisbl <louis@beltramo.me>
2016-01-22 15:00:56 +0100

    Add Mapbox MapView

--- app/src/main/java/fr/louisbl/hellomap/Map.java
+++ app/src/main/java/fr/louisbl/hellomap/Map.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.views.MapView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,8 +61,15 @@ public class Map extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
 
+        MapView mapView = (MapView) view.findViewById(R.id.map_view);
+
+        mapView.setAccessToken("");
+
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.onCreate(savedInstanceState);
+
+        return view;
+    }
 }
